1a29eb2aadb8f8486fc8b2efb4731a6e580ecd7e
Eli <136011323+eliaselliotson@users.noreply.github.com>
2025-09-28 00:40:58 +0000

    Migrate parts of the UI to bootstrap

--- public/js/toastButtons.js
+++
@@ -1,7 +0,0 @@
-const toastButtons = document.querySelectorAll(".toast button");
-
-for (let button of toastButtons) {
-  button.addEventListener("click", () => {
-    button.parentElement.remove();
-  })
-}
